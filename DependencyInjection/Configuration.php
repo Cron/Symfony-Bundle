331f9ef34b2868a3da628feae16e1ce25c946b0e
Dries De Peuter <dries@nousefreak.be>
2014-03-15 14:54:08 +0100

    Remove unused methods

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,12 +17,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cron_cron');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-        return $treeBuilder;
+        return new TreeBuilder();
     }
 }
