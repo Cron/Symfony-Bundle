3b129f794bc44db992cdfd57e4746b68b7001d66
Alexander Lokhman <alex.lokhman@gmail.com>
2017-11-14 14:20:07 +0000

    Add cron start/stop commands

---
+++ Command/CronStartCommand.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This file is part of the SymfonyCronBundle package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cron\CronBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Alexander Lokhman <alex.lokhman@gmail.com>
+ */
+class CronStartCommand extends ContainerAwareCommand
+{
+    const PID_FILE = '.cron-pid';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('cron:start')
+            ->setDescription('Starts cron scheduler')
+            ->addOption('blocking', 'b', InputOption::VALUE_NONE, 'Run in blocking mode.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('blocking')) {
+            $output->writeln(sprintf('<info>%s</info>', 'Starting cron scheduler in blocking mode.'));
+            $this->scheduler($output->isVerbose() ? $output : new NullOutput(), null);
+
+            return 0;
+        }
+
+        if (!extension_loaded('pcntl')) {
+            throw new \RuntimeException('This command needs the pcntl extension to run.');
+        }
+
+        $pidFile = getcwd().'/'.self::PID_FILE;
+
+        if (-1 === $pid = pcntl_fork()) {
+            throw new \RuntimeException('Unable to start the cron process.');
+        } elseif (0 !== $pid) {
+            if (false === file_put_contents($pidFile, $pid)) {
+                throw new \RuntimeException('Unable to create process file.');
+            }
+
+            $output->writeln(sprintf('<info>%s</info>', 'Cron scheduler started in non-blocking mode...'));
+
+            return 0;
+        }
+
+        if (-1 === posix_setsid()) {
+            throw new \RuntimeException('Unable to set the child process as session leader.');
+        }
+
+        $this->scheduler(new NullOutput(), $pidFile);
+    }
+
+    private function scheduler(OutputInterface $output, $pidFile)
+    {
+        $input = new ArrayInput([]);
+
+        $console = $this->getApplication();
+        $command = $console->find('cron:run');
+
+        while (true) {
+            $now = microtime(true);
+            usleep((60 - ($now % 60) + (int) $now - $now) * 1e6);
+
+            if (null !== $pidFile && !file_exists($pidFile)) {
+                break;
+            }
+
+            $command->run($input, $output);
+        }
+    }
+}
---
+++ Command/CronStopCommand.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the SymfonyCronBundle package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cron\CronBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Alexander Lokhman <alex.lokhman@gmail.com>
+ */
+class CronStopCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('cron:stop')
+            ->setDescription('Stops cron scheduler');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $pidFile = getcwd().'/'.CronStartCommand::PID_FILE;
+        if (!file_exists($pidFile)) {
+            return 0;
+        }
+
+        if (!extension_loaded('pcntl')) {
+            throw new \RuntimeException('This command needs the pcntl extension to run.');
+        }
+
+        if (!posix_kill(file_get_contents($pidFile), SIGINT)) {
+            if (!unlink($pidFile)) {
+                throw new \RuntimeException('Unable to stop scheduler.');
+            }
+
+            $output->writeln(sprintf('<comment>%s</comment>', 'Unable to kill cron scheduler process. Scheduler will be stopped before the next run.'));
+
+            return 0;
+        }
+
+        unlink($pidFile);
+
+        $output->writeln(sprintf('<info>%s</info>', 'Cron scheduler is stopped.'));
+    }
+}
