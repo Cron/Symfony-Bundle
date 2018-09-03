32fa53a38513fd8bae31535e2d777fb4e016c975
esynaps <30432290+esynaps@users.noreply.github.com>
2018-09-03 16:09:42 +0200

    Update Manager.php
    support multiple doctrine manager

--- Cron/Manager.php
+++ Cron/Manager.php
@@ -13,6 +13,7 @@ use Cron\CronBundle\Entity\CronJob;
 use Cron\CronBundle\Entity\CronJobRepository;
 use Cron\CronBundle\Entity\CronReport;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @author Dries De Peuter <dries@nousefreak.be>
@@ -20,16 +21,16 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 class Manager
 {
     /**
-     * @var RegistryInterface
+     * @var EntityManagerInterface
      */
-    protected $registry;
+    protected $manager;
 
     /**
      * @param RegistryInterface $registry
      */
-    function __construct(RegistryInterface $registry)
+    function __construct(ManagerRegistry $registry)
     {
-        $this->registry = $registry;
+        $this->manager = $registry->getManagerForClass(CronJob::class);
     }
 
     /**
@@ -37,7 +38,7 @@ class Manager
      */
     protected function getJobRepo()
     {
-        return $this->registry->getRepository('CronCronBundle:CronJob');
+        return $this->manager->getRepository(CronJob::class);
     }
 
     /**
@@ -45,7 +46,6 @@ class Manager
      */
     public function saveReports(array $reports)
     {
-        $em = $this->registry->getManager();
         foreach ($reports as $report) {
             $dbReport = new CronReport();
             $dbReport->setJob($report->getJob()->raw);
@@ -53,9 +53,9 @@ class Manager
             $dbReport->setExitCode($report->getJob()->getProcess()->getExitCode());
             $dbReport->setRunAt(\DateTime::createFromFormat('U.u', (string) $report->getStartTime()));
             $dbReport->setRunTime($report->getEndTime() - $report->getStartTime());
-            $em->persist($dbReport);
+            $this->manager->persist($dbReport);
         }
-        $em->flush();
+        $this->manager->flush();
     }
 
     /**
@@ -87,9 +87,8 @@ class Manager
      */
     public function saveJob(CronJob $job)
     {
-        $em = $this->registry->getManager();
-        $em->persist($job);
-        $em->flush();
+        $this->manager->persist($job);
+        $this->manager->flush();
     }
 
     /**
@@ -109,8 +108,7 @@ class Manager
      */
     public function deleteJob(CronJob $job)
     {
-        $em = $this->registry->getManager();
-        $em->remove($job);
-        $em->flush();
+        $this->manager->remove($job);
+        $this->manager->flush();
     }
 }
