4d9b35108fb17b9677242eb7d024b9c2187e4d48
Wouter van der Loop | Toppy <40226924+wouter-toppy@users.noreply.github.com>
2021-11-26 11:55:15 +0100

    Pass initial max_execution_time to subprocess

--- Cron/CommandBuilder.php
+++ Cron/CommandBuilder.php
@@ -44,6 +44,6 @@ class CommandBuilder
      */
     public function build($command)
     {
-        return sprintf('%s %s %s %s --env=%s', $this->phpExecutable, ' --define memory_limit='.ini_get('memory_limit'), $_SERVER['SCRIPT_NAME'], $command, $this->environment);
+        return sprintf('%s %s %s %s --env=%s', $this->phpExecutable, ' --define max_execution_time='.ini_get('max_execution_time').' --define memory_limit='.ini_get('memory_limit'), $_SERVER['SCRIPT_NAME'], $command, $this->environment);
     }
 }
