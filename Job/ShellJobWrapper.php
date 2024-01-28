c0a6703f44f3b40677b5e7d370d975302bc9b87c
Denis Golubovskiy <bukashk0zzz@me.com>
2024-01-28 23:06:13 +0100

    Fix issue #133 Symfony 7 not run jobs (#134)
    * Fix issue #133 Symfony 7 not run jobs
    
    * Fix issue Cron#133 Symfony 7 not run jobs

--- Job/ShellJobWrapper.php
+++ Job/ShellJobWrapper.php
@@ -4,8 +4,15 @@ namespace Cron\CronBundle\Job;
 
 use Cron\CronBundle\Entity\CronJob;
 use Cron\Job\ShellJob;
+use Symfony\Component\Process\Process;
 
 class ShellJobWrapper extends ShellJob
 {
     public ?CronJob $raw = null;
+
+    public function setCommand($command)
+    {
+        $commandArray = explode(' ', $command);
+        $this->process = new Process($commandArray);
+    }
 }
