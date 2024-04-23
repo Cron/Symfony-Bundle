84bc680b634ad15c34f52e3bb24271f3fb8f73ac
Anastasiia <anastasia.vinnytska@gmail.com>
2024-04-23 18:45:20 +0200

    use array for command arument (fix #133 issue)

--- Job/ShellJobWrapper.php
+++ Job/ShellJobWrapper.php
@@ -4,8 +4,16 @@ namespace Cron\CronBundle\Job;
 
 use Cron\CronBundle\Entity\CronJob;
 use Cron\Job\ShellJob;
+use Symfony\Component\Process\Process;
 
 class ShellJobWrapper extends ShellJob
 {
     public ?CronJob $raw = null;
+
+    public function setCommand($command, $cwd = null, ?array $env = null, $input = null, $timeout = 60, array $options = [])
+    {
+        $commandArray = explode(' ', $command);
+
+        $this->process = new Process($commandArray);
+    }
 }
